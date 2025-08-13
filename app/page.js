271b7f9909648a51716029b75a37f6d7cd3addac
Mansoor khan <107978052+mansoor369@users.noreply.github.com>
2025-08-12 21:55:21 -0700

    changed link

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans  flex justify-center items-center mt-48">
-      <iframe title="Seats Analysis" width="1240" height="600.25" src="https://app.powerbi.com/reportEmbed?reportId=7835cfe6-8897-4b0b-8b3b-62b2254a1264&autoAuth=true&ctid=484eedba-e5ac-4bd5-85c5-fc4c8ba6efb9" frameborder="0" allowFullScreen="true"></iframe>
-    </div>
+<iframe title="Seats Analysis" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=041fbbe0-7cb1-44a8-ba1a-06cc81250406&autoAuth=true&ctid=8fcff337-c726-45dd-8d98-c8ba4db7cc15" frameborder="0" allowFullScreen="true"></iframe>    </div>
   );
 }
