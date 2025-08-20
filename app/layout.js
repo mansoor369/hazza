ccc9b5d00f6e1e0e07e0e2501bfc0c1fdf3e22a2
Mansoor khan <107978052+mansoor369@users.noreply.github.com>
2025-08-20 15:52:49 +0500

    added new visual

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`font-[Nunito] `}
+        className={`font-[Nunito]`}
       >
         {children}
       </body>
